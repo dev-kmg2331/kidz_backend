7516442e63a0bedbbf3be005932fcd9cfa3c231d
kmg2331 <kmg2331@gmail.com>
2023-04-02 01:27:39 +0900

    kidz app initial commit

---
+++ kidz/src/test/java/com/app/kidz/KidzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.kidz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
