f378a8b7663baa3f6cfc160e47a964f357c09b0b
dev-kmg2331 <kmg2331@gmail.com>
2023-04-02 04:58:34 +0900

    엔티티로 DB 테이블 생성, 및 엔티티 필드에 DB 컬럼 속성 부여

--- kidz/src/main/java/com/app/kidz/helloJpa/JpaMain.java
+++ kidz/src/main/java/com/app/kidz/helloJpa/JpaMain.java
@@ -6,7 +6,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 @Slf4j
 public class JpaMain {
@@ -23,44 +22,16 @@ public class JpaMain {
 
         try {
 
-            /* 비영속 */
-//            Member member = new Member();
-            // 1차 캐시에 저장됨
-//            member.setName("HELLOPERSISTANCE");
 
-            /* 영속 */
-            // 내부적으로 flush()라는 메소드를 호출한 뒤,
-            // INSERT SQL 을 생성한 후 쓰기 지연 SQL 저장소에 저장한다.
-            // 또한, 1차 캐시라는 영역에 Entity가 저장된다.
-            // 이후 transaction을 commit 한 시점에서 저장한 SQL들이 날라간다.
-//            entityManager.persist(member);
+            Member member = new Member();
 
-            // 1차 캐시에서 조회함 -> 영속 엔티티의 동일성 보장
-            Member foundMember = entityManager.find(Member.class, 1L);
+            member.setUsername("으악");
+            member.setRoleType(RoleType.ADMIN);
 
-            // JPA는 내부적으로 1차 캐시라는 영역에 @Id, Entity 그리고 스냅샷이라는 공간이 있다.
-            /*
-            * -- Id --- Entity --- 스냅샷 --
-            * |      |          |         |
-            * |------|----------|---------|
-            */
-            // 스냅샷에는 값을 읽어온 최초 시점의 상태를 저장하는 공간이다.
-            // EntityManager는 Entity와 스냅샷을 비교하여 변경되었다면 UPDATE SQL을 생성한다.
-            // 이후 생성된 UPDATE SQL은 쓰기 지연 SQL 저장소에 저장, flush() 메소드가 사용될 때, 호출 된다.
-            // 이후 commit 시점에서 DB에 반영된다.
-            foundMember.setName("엔티티 변경 감지");
+            entityManager.persist(member);
 
-            /* 준영속 상태 */
-            // EntityManager가 관리하지 않는 상태.
-//            entityManager.detach(foundMember);
-
-            /* 영속성 컨텍스트 초기화 */
-//            entityManager.clear();
-
-            /* 영속성 컨텍스트 종료 */
-//            entityManager.close();
-
-//            entityTransaction.commit();
+            // 엔티티와 DB 테이블을 비교해서 create, update, validate 등의 옵션을 줄 수 있다.
+            entityTransaction.commit();
         } catch (Exception e) {
             entityTransaction.rollback();
         } finally {
---
+++ kidz/src/main/java/com/app/kidz/helloJpa/RoleType.java
@@ -0,0 +1,5 @@
+package com.app.kidz.helloJpa;
+
+public enum RoleType {
+    USER, ADMIN
+}
